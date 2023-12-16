04c045441df940db5e84a2bf78f83f56d90c626f
RomeuCajamba <romeucajamba@gmail.com>
2023-12-17 00:19:25 +0100

    Criou-se o themeprovider e estilo global

--- transacoes/src/App.tsx
+++ transacoes/src/App.tsx
@@ -1,10 +1,16 @@
+import { ThemeProvider } from "styled-components";
+import { defaultTheme } from "./styles/theme/default";
 
 
 export function App() {
 
   return (
-   <>
-    <p>Olá mundo React</p>
-   </>
+   //ThemeProvider é o provider de contexto do styled-components
+   //Ele vai prove o tema que vamos usar ao longo da nossa aplicação
+   <ThemeProvider theme={defaultTheme}>
+     <p>Olá mundo React</p>
+   </ThemeProvider>
+
+  
   )
 }
