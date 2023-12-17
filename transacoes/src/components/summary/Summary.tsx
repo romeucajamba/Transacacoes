c15548dc62bfb03c59c14dc999c88c5e3888a0d5
RomeuCajamba <romeucajamba@gmail.com>
2023-12-17 02:39:00 +0100

    estilização do cards

--- transacoes/src/components/summary/Summary.tsx
+++ transacoes/src/components/summary/Summary.tsx
@@ -1,33 +1,33 @@
-import {SummaryContainer} from './styles';
+import {SummaryContainer, SummaryCard} from './styles';
 import {ArrowCircleUp, ArrowCircleDown, CurrencyCircleDollar} from 'phosphor-react';
 
 
 export function Summary(){
     return(
         <SummaryContainer>
-            <div>
+            <SummaryCard>
                 <header>
                     <span>Entrada</span>
                     <ArrowCircleUp size={32} color='#00b37e'/>
                 </header>
                 <strong>17.400.00 KZ</strong>
-            </div>
+            </SummaryCard>
 
-            <div>
+            <SummaryCard>
                 <header>
                     <span>Saídas</span>
                     <ArrowCircleDown size={32} color='#f75a68'/>
                 </header>
                 <strong>17.400.00 KZ</strong>
-            </div>
+            </SummaryCard>
 
-            <div>
+            <SummaryCard variant='green'>
                 <header>
                     <span>Total</span>
-                    <CurrencyCircleDollar size={32} color='#fff'/>
+                    <CurrencyCircleDollar size={32} color='#d1d414'/>
                 </header>
                 <strong>17.400.00 KZ</strong>
-            </div>
+            </SummaryCard>
         </SummaryContainer>
     )
 }
