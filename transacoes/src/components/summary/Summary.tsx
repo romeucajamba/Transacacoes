46eb2cb59b0d7b6f8498f73510ec0b8636b48d5f
RomeuCajamba <romeucajamba@gmail.com>
2023-12-17 02:18:26 +0100

    criação da sections de movimentação

--- transacoes/src/components/summary/Summary.tsx
+++ transacoes/src/components/summary/Summary.tsx
@@ -1,4 +1,5 @@
 import {SummaryContainer} from './styles';
+import {ArrowCircleUp, ArrowCircleDown, CurrencyCircleDollar} from 'phosphor-react';
 
 
 export function Summary(){
@@ -7,7 +8,25 @@ export function Summary(){
             <div>
                 <header>
                     <span>Entrada</span>
+                    <ArrowCircleUp size={32} color='#00b37e'/>
                 </header>
+                <strong>17.400.00 KZ</strong>
+            </div>
+
+            <div>
+                <header>
+                    <span>Saídas</span>
+                    <ArrowCircleDown size={32} color='#f75a68'/>
+                </header>
+                <strong>17.400.00 KZ</strong>
+            </div>
+
+            <div>
+                <header>
+                    <span>Total</span>
+                    <CurrencyCircleDollar size={32} color='#fff'/>
+                </header>
+                <strong>17.400.00 KZ</strong>
             </div>
         </SummaryContainer>
     )
--- transacoes/src/pages/trasations/index.tsx
+++ transacoes/src/pages/trasations/index.tsx
@@ -1,9 +1,11 @@
 import {Header} from '../../components/hedear/Header';
+import {Summary} from '../../components/summary/Summary';
 
 export function Transations(){
     return(
         <div>
             <Header/>
+            <Summary/>
         </div>
     )
 }
