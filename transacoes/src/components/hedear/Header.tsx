eade280c6bb121a3f345c782b31e362058b116a4
RomeuCajamba <romeucajamba@gmail.com>
2023-12-17 13:24:32 +0100

    Instalação da biblioteca radix

--- transacoes/src/components/hedear/Header.tsx
+++ transacoes/src/components/hedear/Header.tsx
@@ -1,11 +1,15 @@
 import {HeaderContainer, HeaderContent, ImageLogo, NewTransationsButton} from './styles';
 import logo from '../../assets/img-150.png';
+import * as Dialog from '@radix-ui/react-dialog';
+
 
 export function Header(){
     return(
         <HeaderContainer>
             <HeaderContent>
                 <ImageLogo  src={logo} alt="logoMoney" />
+
+
                 <NewTransationsButton>Nova transação</NewTransationsButton>
             </HeaderContent>
         </HeaderContainer>
