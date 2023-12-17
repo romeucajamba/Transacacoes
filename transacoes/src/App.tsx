7247299a6dc5d7bb859332ca908889916f440b03
RomeuCajamba <romeucajamba@gmail.com>
2023-12-17 01:05:37 +0100

    criou-se componentes header e transations

--- transacoes/src/App.tsx
+++ transacoes/src/App.tsx
@@ -1,6 +1,6 @@
 import { ThemeProvider } from "styled-components";
 import { defaultTheme } from "./styles/theme/default";
-
+import { GlobalStyle } from "./styles/global";
 
 export function App() {
 
@@ -8,6 +8,7 @@ export function App() {
    //ThemeProvider é o provider de contexto do styled-components
    //Ele vai prove o tema que vamos usar ao longo da nossa aplicação
    <ThemeProvider theme={defaultTheme}>
+    <GlobalStyle/>
      <p>Olá mundo React</p>
    </ThemeProvider>
 
